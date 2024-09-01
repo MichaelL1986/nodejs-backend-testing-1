2647cca30705ebade4894335618df6dedc48388d
MichaelL1986 <167879507+MichaelL1986@users.noreply.github.com>
2024-09-01 20:59:30 +0300

    tests implemented

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,22 +1,34 @@
-import { Post, PostsService } from './posts.service';
+import { Post, PostsService } from "./posts.service";
 
-describe('PostsService', () => {
+describe("PostsService", () => {
   let postsService: PostsService;
-  const post: Omit<Post, 'id' | 'date'> = {
-    text: 'Mocked post',
+  const post: Omit<Post, "id" | "date"> = {
+    text: "Mocked post",
   };
+  let preExistingPost: Post;
 
   beforeEach(async () => {
     postsService = new PostsService();
 
-    postsService.create({ text: 'Some pre-existing post' });
+    preExistingPost = postsService.create({
+      text: "Some pre-existing post",
+    });
   });
 
-  it('should add a new post', () => {
-    // реализуйте тест-кейс
+  it("should add a new post", () => {
+    // должен быть метод getAll для корректного теста
+    const postToAdd = post;
+
+    const createdPost = postsService.create(postToAdd);
+
+    expect(createdPost).toMatchObject(postToAdd);
   });
 
-  it('should find a post', () => {
-    // реализуйте тест-кейс
+  it("should find a post", () => {
+    const { id } = preExistingPost;
+
+    const findedPost = postsService.find(id);
+
+    expect(findedPost).toMatchObject(preExistingPost);
   });
 });
